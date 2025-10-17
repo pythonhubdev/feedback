23f1cd9bd8e731218d38ee515837714af27aa215
Vetrichelvan <pythonhubdev@gmail.com>
2025-10-17 13:12:31 +0530

    fix: checkout logs if the connection string is proper

--- src/integrations/supabase/supabase.ts
+++ src/integrations/supabase/supabase.ts
@@ -1,8 +1,11 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import { Effect } from "effect";
 import postgres from "postgres";
 import { feedbackTable } from "~/core/models/feedback.ts";
 
 const connectionString = process.env.SUPABASE_URL || "";
+
+Effect.log(`Supabase Connection String: ${connectionString}`);
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString, { prepare: false });
